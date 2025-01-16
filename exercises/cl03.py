5ec7e568ef453829cf923c1a033034b12796734d
marybealin <mlinares@unc.edu>
2025-01-16 09:05:09 -0500

    backing up ex00 after completion

---
+++ exercises/cl03.py
@@ -0,0 +1,15 @@
+def celsius_to_fahrenheit(degrees: int) -> float:
+    """convert degrees celsius to degrees fahrenheit"""
+    return (degrees * 9 / 5) + 32
+
+
+celsius_to_fahrenheit(degrees=0)
+
+
+def perimeter(length: float, width: float) -> float:
+    """calculate perimeter of a rectangle"""
+    return (length * 2) + (width * 2)
+
+
+perimeter(34.5, 39)
+print(perimeter)
